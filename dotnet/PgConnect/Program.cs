6b112859c1b33a8a037eed662339b51d9a43875f
AlexDemokidov <AlexDemokidov>
2023-12-16 14:35:54 +0300

    sdl1

---
+++ dotnet/PgConnect/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Npgsql;
+
+Console.WriteLine("User is ?");
+var userName = Console.ReadLine();
+Console.WriteLine("Password is ?");
+var password = Console.ReadLine();
+
+Console.WriteLine($"User is {userName}");       //user
+Console.WriteLine($"Password is {password}");   //userpass
+
+StreamReader sr = new StreamReader("../../../sdl1.conf");
+var connectionString = sr.ReadLine();
+
+Npgsql.NpgsqlConnectionStringBuilder csb = new Npgsql.NpgsqlConnectionStringBuilder(connectionString);
+
+csb.Username = userName;
+csb.Password = password;
+
+await using var dataSource = NpgsqlDataSource.Create(csb);
+
+// Retrieve all rows
+await using (var cmd = dataSource.CreateCommand("SELECT VERSION();"))
+await using (var reader = await cmd.ExecuteReaderAsync())
+{
+    while (await reader.ReadAsync())
+    {
+        Console.WriteLine(reader.GetString(0));
+    }
+}
